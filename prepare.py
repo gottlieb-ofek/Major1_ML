9f0f64066fe915404e072100b60d2d9992937f65
gottlieb-ofek <82642364+gottlieb-ofek@users.noreply.github.com>
2022-11-27 22:47:02 +0200

    hi

---
+++ prepare.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+
+"this function makes features to numerics and extracts features from other features"
+class MakeNumericAndExtract:
+
+    def __int__(self, df):
+        self.df = df
+
+    "here are the features we convert to numeric / boolean"
+
+
+
+
+
+
+
+
+
+#TODO - main function
+def prepare_data(training_data, new_data):
+
+
+    return
